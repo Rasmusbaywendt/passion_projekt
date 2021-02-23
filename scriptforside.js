64315de1d935dac3c87361c08a76473ee82e34e5
SasiaDagnoli <sasi0026@stud.kea.dk>
2021-02-23 10:20:03 +0100

    minus overflow på forsiden

---
+++ scriptforside.js
@@ -0,0 +1,22 @@
+window.addEventListener("load", sidenVises);
+
+function sidenVises() {
+    console.log("sidenVises");
+    document.querySelector("#menuknap").addEventListener("click", toggleMenu);
+}
+
+function toggleMenu() {
+    console.log("toggleMenu")
+
+    document.querySelector("#burger").classList.toggle("gone");
+
+    let erSkjult = document.querySelector("#burger").classList.contains("gone");
+
+    if (erSkjult == true) {
+        document.querySelector("#menuknap").textContent = "☰";
+        //document.querySelector("body").classList.remove("fixed");
+    } else {
+        document.querySelector("#menuknap").textContent = "╳";
+        //document.querySelector("body").classList.add("fixed");
+    }
+}
